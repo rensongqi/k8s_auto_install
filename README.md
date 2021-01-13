1d519d810bd604a3d39f226ceadab6cb7bc85834
rensongqi <songqi@51vr.local>
2021-01-13 22:30:51 +0800

    Add some files.

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# k8s_auto_install
+# K8s-v1.18.3 auto install in 5 min.
+
+
+
+## 1. Git clone
+
+```bash
+git clone https://github.com/rensongqi/k8s_auto_install.git
+```
+
+
+
+## 2. Exec k8s_auto_install.sh
+
+```
+cd k8s_auto_install
+sudo chmod +x k8s_auto_install.sh
+./k8s_auto_install.sh
+```
+
+
+
+## 3. You need to select the type of network plug-in during installation
+
+![image-20210113200440686](C:\Users\songqi\AppData\Roaming\Typora\typora-user-images\image-20210113200440686.png)
+
+
+
+## 4. Docker image registry
+
+```bash
+# Contains images related to K8S, Calico & Flannel
+registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/
+```
+
+
+
+## 5 Node join cluster
+
+```bash
+# 1. Get token
+kubeadm token list | awk 'NR==2{print $1}'
+
+# 2. Get k8s ca hash
+openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
+
+# 3. Node join k8s cluster
+kubeadm join 192.168.1.1:6443 --token ayhdt4.u83rp3re562iwvwu --discovery-token-ca-cert-hash sha256:1ff60a352c181ead103cb3f76fc328115bdb7e5c705d2348e8900df0ecec297e --ignore-preflight-errors=Swap
+```
+
+
+
+## 6. My blog
+
+```
+https://blog.csdn.net/Mr_rsq
+```
+
---
+++ k8s_auto_install.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+# Author: Rensongqi
+# Email: cactirsq@163.com
+
+# step 1. copy repos & config hosts file
+\cp ./repos/* /etc/yum.repos.d/
+
+IP=`hostname -I | awk '{print $1}'`
+cat >>/etc/hosts<<EOF
+$IP master
+EOF
+
+# step 2. Close firewalld & selinux
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+sudo setenforce 0
+sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+# step 3. Close swap
+sudo swapoff -a
+sudo sed -ri 's/.*swap.*/#&/' /etc/fstab
+
+# step 4 Config ipvs
+sudo yum install ipvsadm -y
+sudo chmod +x ./scripts/ipvs.sh
+sudo bash ./scripts/ipvs.sh
+sudo lsmod | grep ip_vs
+line=`lsmod | grep ip_vs | wc -l`
+if [ $line -gt 3 ]
+then
+    echo "************************ipvs load successfully!************************"
+    echo ""
+    echo ""
+else
+    echo "************************ipvs load failed!************************"
+    exit 1
+fi
+
+# step 5. Get kubelet kubeadm pkg and so on ...
+sudo yum install kubelet-1.18.3-0.x86_64 kubeadm-1.18.3-0.x86_64 kubectl-1.18.3-0.x86_64 docker-ce -y
+sudo systemctl enable kubelet && sudo systemctl enable docker && sudo systemctl start docker
+
+# step 6. Config docker daemon.json
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://3po4uu60.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+echo ""
+echo ""
+
+# step 7. Download K8s images
+echo "************************K8s images Downloading************************"
+for i in kube-apiserver:v1.18.3 kube-controller-manager:v1.18.3 kube-scheduler:v1.18.3 kube-proxy:v1.18.3 pause:3.2 etcd:3.4.3-0 coredns:1.6.7
+do
+    docker pull registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/$i
+done
+
+while :
+do
+	echo ""
+	echo ""
+	echo "************************Network plugin install************************"
+    read -p "Please enter the network plugin you want to download [flannel|calico]: " name
+    if [ "$name" == "flannel" ]
+    then
+        echo "Please wait a moment..."
+        docker pull registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/flannel:v0.13.1-rc1
+        break
+    elif [ "$name" == "calico" ]
+    then
+        echo "Please wait a moment..."
+        docker push registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/node:v3.8.2
+        docker push registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/cni:v3.8.2
+        docker push registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/kube-controllers:v3.8.2
+        docker push registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/pod2daemon-flexvol:v3.8.2
+        break
+    else
+        echo "Your input error, please enter [flannel|calico]"
+    fi
+done
+
+echo ""
+echo "************************Change docker tag************************"
+if [ "$name" == "flannel" ]
+then
+	sudo docker tag registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/flannel:v0.13.1-rc1 quay.io/coreos/flannel:v0.13.1-rc1
+	sudo docker rmi registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/flannel:v0.13.1-rc1
+elif [ "$name" == "calico" ]
+then
+    for i in node:v3.8.2 cni:v3.8.2 kube-controllers:v3.8.2 pod2daemon-flexvol:v3.8.2
+    do
+        sudo docker tag registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/$i calico/$i
+	    sudo docker rmi registry.cn-shanghai.aliyuncs.com/rsq_k8s_images/$i
+    done
+fi
+
+sudo docker images |grep rsq_k8s_images |awk '{print "docker tag",$1":"$2,$1":"$2}' |sed -e 's#registry.cn-shanghai.aliyuncs.com/rsq_k8s_images#k8s.gcr.io#2' | sh -x
+echo ""
+echo "************************Docker images download successfully************************"
+
+
+# step 8. Open iptables rule and ip_forward
+sudo echo 1 > /proc/sys/net/bridge/bridge-nf-call-ip6tables 
+sudo echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+sudo echo 1 > /proc/sys/net/ipv4/ip_forward
+sudo cat >>/etc/rc.local<<EOF
+echo 1 > /proc/sys/net/bridge/bridge-nf-call-ip6tables 
+echo 1 > /proc/sys/net/bridge/bridge-nf-call-iptables
+echo 1 > /proc/sys/net/ipv4/ip_forward
+EOF
+
+# step 9. Kubeadm init
+sudo kubeadm init --config ./scripts/kubeadm-config-latest.yaml --ignore-preflight-errors=Swap
+
+if [ "$?" != 0 ] ; then
+   echo "kubeadm init Failed!!!"
+   exit 2
+fi
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# step 10. Apply network plugin
+if [ "$name" == "flannel" ]
+then
+	sudo kubectl apply -f ./plugins/flannel_v0.13.1/kube-flannel.yml
+elif [ "$name" == "calico" ]
+then
+	sudo kubectl apply -f ./plugins/calico_v3.17.0/tigera-operator.yaml
+	sudo kubectl apply -f ./plugins/calico_v3.17.0/custom-resources.yaml
+fi
+
+# step 11. Config Master schedulable
+# sudo kubectl taint nodes --all node-role.kubernetes.io/master-
